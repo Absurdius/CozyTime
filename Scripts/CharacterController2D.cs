4e1c25e08b3bc1ea99690dbe8918225339c579e2
Absurdius <e94doclrd@hotmail.com>
2023-10-29 22:23:19 +0100

    Added character animation for movement directions

--- Scripts/CharacterController2D.cs
+++ Scripts/CharacterController2D.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction {
+    UP = 0,
+    DOWN = 1,
+    // LEFT AND RIGHT MUST BE GREATER THAN 1 BECAUSE OF THE ANIMATOR
+    LEFT = 2,
+    RIGHT = 3,
+}
+
 public class CharacterController2D : MonoBehaviour
 {
     public float speed = 1.0f;
 
     private Transform transform;
     private Vector2 movement = Vector2.zero; 
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         transform = GetComponent<Transform>();
+        this.animator = GetComponent<Animator>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,9 +32,33 @@ public class CharacterController2D : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
         movement.Normalize();
+     
+        Move(movement);
+        SetAnimation();
+    }
 
-        transform.Translate(movement * speed * Time.deltaTime);
+    private void Move(Vector2 movement)
+    {
+        transform.Translate(speed * Time.deltaTime * movement);
+    }
+
+    private void SetAnimation()
+    {
+        if(movement.x > 0.5f) {
+            // RIGHT
+            spriteRenderer.flipX = false;
+            animator.SetInteger("direction", (int) Direction.RIGHT);
+        } else if (movement.x < -0.5) {
+            // LEFT
+            spriteRenderer.flipX = true;
+            animator.SetInteger("direction", (int)Direction.LEFT);
+        } else if (movement.y > 0.5) {
+            // UP 
+            animator.SetInteger("direction", (int)Direction.UP);
+        } else if (movement.y < -0.5){
+            // DOWN
+            animator.SetInteger("direction", (int)Direction.DOWN);
+        }
     }
 }
